688cc75a1aeebc19904b1d36f7b9dee6523142b9
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-03-23 12:20:30 +0000

    topFormStyled

---
+++ components/data/brands.js
@@ -0,0 +1,20 @@
+export default [
+    {brandName: 'Arc Minute', hourlyRate: 15, rebarcodingRate: 0.25},
+    {brandName: 'Bettina Ceramica', hourlyRate: 17, rebarcodingRate: 0.30},
+    {brandName: 'Blake London', hourlyRate: 17, rebarcodingRate: 0.25},
+    {brandName: 'Day Dress', hourlyRate: 17, rebarcodingRate: 0.25},
+    {brandName: 'Futsol', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'Hadeda', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'House Of Silviano', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'Pinky City Prints', hourlyRate: 17, rebarcodingRate: 0.25},
+    {brandName: 'Sirplus', hourlyRate: 17, rebarcodingRate: 0.25},
+    {brandName: 'Spry Workwear', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'Thread And Tonic', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'The Wholeleaf Company', hourlyRate: 25, rebarcodingRate: 5},
+    {brandName: 'Faceplant', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'The Colombia Collective', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'Labell-D', hourlyRate: 25, rebarcodingRate: 0.30},
+    {brandName: 'Thom', hourlyRate: 20, rebarcodingRate: 0.30},
+
+
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from 'next'
 import { useState } from 'react'
 import Image from 'next/image'
+import logoOrange from '../public/logoOrange.png'
 import Layout from '../components/Layout'
 import Extra from '../components/FormFields/module'
 import workOrders from '../components/FormFields/workOrders'
+import brands from '../components/data/brands'
 
 let extraFieldsLookup: object
 extraFieldsLookup = {
@@ -53,24 +55,73 @@ const IndexPage: NextPage = () => {
     }
   }
 
+//   const sortBrands = (brandsArray: Array) => {
+
+//   }
+
+//   let userTestStatus: { id: number, name: string }[] = [
+//     { "id": 0, "name": "Available" },
+//     { "id": 1, "name": "Ready" },
+//     { "id": 2, "name": "Started" }
+// ];
+
+
+
   return (
     <Layout title="Submit Your Work Order | Tu Pack">
-     <body class="font-mono bg-black">
-        <div class="container mx-auto">
-          <div class="flex justify-center px-6 my-12">
-            <div class="w-full xl:w-3/4 lg:w-11/12 flex">
+        <body class="bg-black">
+            <div class="flex min-h-screen items-center justify-center">
 
-							<div class="text-center">
-								<a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./index.html"
-								>
-                    Already have an account? Login!
-                  </a>
+                <div class="bg-white border border-none p-6 rounded-2xl">
+
+                    <div class="mx-4 sm:mx-24 md:mx-34 lg:mx-56 mx-auto  flex items-center space-y-4 py-16 font-semibold text-gray-500 flex-col">
+                            <div class="w-4/5">
+                                <Image src={logoOrange} layout="responsive"  alt="pic" />
+                                <h1 class="text-black text-center text-2xl">Submit Your Work Order</h1>
+                            </div>
+
+                        <form className="orderAuth flex flex-col  space-y-5 pt-4 pb-10" >  
+                            <h1 class="text-black text-2xl">Start Here</h1>
+
+                        <select required class="w-full p-2 bg-black rounded-md  border border-gray-700 focus:border-blue-700"
+                        name="brand" id="brand" onChange={set('brand')}>
+                            <option hidden disabled selected>Brand Name</option>
+                            {brands.sort(function(a,b){
+                              if(a.brandName < b.brandName) { return -1; }
+                              if(a.brandName > b.brandName) { return 1; }
+                              return 0;
+                              }).map( 
+                                    ({brandName})  =>  <option value={brandName}>{brandName}</option>
+                                )
+                            }
+                        </select>
+                               
+                        <input required class="w-full p-2 bg-gray-900 rounded-md  border border-gray-700 focus:border-blue-700"
+                         id='name' onChange={set('name')}  placeholder="Your Name" type="text" />
+                        
+                        <input required class="w-full p-2 bg-gray-900 rounded-md  border border-gray-700 focus:border-blue-700"
+                         id='number' placeholder="Contact Number" type="tel" />
+
+
+                        <select required class="w-full p-2 bg-black rounded-md  border border-gray-700 focus:border-blue-700"
+                         name="order" id="orderMenu" onChange={(e) => handleWorkOrder(e.target.value)}>
+                          <option hidden disabled selected>Choose a Work Order</option>
+                          {workOrders.map( ({order})  =>  <option value={order}>{order}</option>
+                          )}
+                        </select>
+
+                        {extraFields}
+                        
+                        <input class="w-full p-2 bg-black hover:bg-amber-500 rounded-full font-bold text-white hover:text-black border border-gray-700 cursor-pointer"
+                            type="submit" />
+
+                        </form>
+                    </div>
+
+
                 </div>
+
             </div>
-          </div>
-        </div>
     </body>
 
     </Layout>
