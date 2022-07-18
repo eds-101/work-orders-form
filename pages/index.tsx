203a163e8488eb284f9d58291b6bb310db004b7b
devlance22 <fiverrfreelance19@gmail.com>
2022-07-19 00:35:04 +0500

    Submit Zendesk ticket on form submission

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,6 +61,44 @@ const IndexPage: NextPage = () => {
     setExtraFields(component);
   };
 
+  const submitZendeskTicket = async (primaryData: any, extraFields: any) => {
+    const subdomain = process.env.NEXT_PUBLIC_ZENDESK_SUBDOMAIN;
+    const formData = {...primaryData, ...extraFields}
+    const comment = Object.keys(formData).reduce(function(res, key) {
+        return res.concat(` ${key} : ${formData[key]}, `);
+    }, "");
+
+    const data = {
+      "request": {
+        "requester": {
+            "name": primaryData['name'],
+            "email": primaryData['email']
+        },
+        "subject": `New Order: ${primaryData['tracking_id']} `,
+        "comment": {
+            "body": comment
+        }
+      }
+    }
+
+    try {
+      const headers = {
+        "Content-Type": "application/json"
+      };
+      const response = await fetch(
+        `https://${subdomain}.zendesk.com/api/v2/requests.json`,
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers
+        }
+      );
+      return await response.json();
+    } catch (error) {
+      return error;
+    }
+  };
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     let insertData: any;
@@ -143,6 +181,10 @@ const IndexPage: NextPage = () => {
       'specific_fields'
     );
     console.log(extraData);
+
+    // Submit Zendesk Ticket
+    await submitZendeskTicket(insertData, specificFields);
+
     alert('Form submitted successfully');
     Router.push({
       pathname: `/submitted/${order_id}`,
