2ba9e57b7923c4e56f98eab45bb04be52d248d3a
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-07-25 19:47:00 +0100

    work order submitted page

--- pages/submitted/[id].tsx
+++ pages/submitted/[id].tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import Layout from '../../components/Layout';
 import { supabaseClient } from '../../lib/client';
+import logoOrange from '../../public/logoOrange.png';
 
 const FinishedPage: NextPage = (props) => {
   const { data } = props;
@@ -10,34 +11,44 @@ const FinishedPage: NextPage = (props) => {
       {data && (
         <>
           <Layout title="Work Order Submitted | Work Management System | TuPack" />
-          <div className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
-            <h1>Work Order Submitted</h1>
 
-            <main
-              role="main"
-              className="w-full lg:w-5/6 flex-grow pt-1 px-3"
-            >
-              <p>
-                Thanks for submitting your order, email confirmation
-                has been sent.
-              </p>
-              <p>
-                Please note your Order Number below which you use to
-                login into the tracker. It's also been emailed to you.
-              </p>
-
-              <h3>Order Number: {data.tracking_id}</h3>
-              <strong>Click here for link to tracker</strong>
-              {/* // link to tracker homepage */}
-            </main>
-            <div className="w-fixed w-full flex-shrink flex-grow-0 px-2"></div>
+          <div className="items-center flex flex-wrap">
+            <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
+              <img
+                alt="..."
+                className="max-w-full rounded-lg shadow-lg"
+                src="https://images.pexels.com/photos/5119209/pexels-photo-5119209.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              />
+            </div>
+            <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+              <div className="md:pr-12">
+                <div className="text-center inline-flex items-center justify-center w-24 h-24 mb-1 mt-8">
+                  <img src={logoOrange} alt="logo" />
+                </div>
+                <h3 className="text-5xl font-semibold">
+                  Work Order Submitted
+                </h3>
+                <h3>Order Number: {data.tracking_id}</h3>
+                <strong>Click here for link to tracker</strong>
+                {/* // link to tracker homepage */}
+                <p className="mt-4 leading-relaxed text-2xl font-semibold">
+                  Thanks for submitting your order, email confirmation
+                  has been sent.
+                </p>
+                <p className="mt-4 leading-relaxed text-2xl font-semibold">
+                  Please note your Order Number below which you use to
+                  login into the tracker. It's also been emailed to
+                  you.
+                </p>
+              </div>
+            </div>
           </div>
         </>
       )}
+      ;
     </>
   );
 };
-
 export default FinishedPage;
 
 export async function getServerSideProps(context: any) {
